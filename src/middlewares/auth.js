0767bebf40443b39a18dd7503803293574958142
Deepanshu-Mehta <146458164+Deepanshu-Mehta@users.noreply.github.com>
2024-12-20 08:33:47 +0530

    midd

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,23 +1,32 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/users.models');
-const AuthVerify = async(req, res,next)=>{
-    try{
-        const {token} = req.cookies;
-        if(!token){
-            return res.status(401).send("Please Login!");
-        }
-        const decoded = await jwt.verify(token, process.env.JWT_SECRET);
-        const {_id} = decoded;
-        const user = await User.findById(_id);
-        if (!user) {
-        throw new Error("User not found");
-        }
 
-        req.user = user;
-        next();
-    }catch(err){
-        res.status(400).send("ERROR: " + err.message);
+const AuthVerify = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      return res.status(401).send("Please login to access this resource.");
     }
-    
-}
-module.exports = AuthVerify; 
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const { _id } = decoded;
+
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(401).send("User not found. Please login again.");
+    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    if (err.name === 'TokenExpiredError') {
+      return res.status(401).send("Session expired. Please login again.");
+    }
+    if (err.name === 'JsonWebTokenError') {
+      return res.status(401).send("Invalid token. Please login again.");
+    }
+    res.status(500).send("Internal server error: " + err.message);
+  }
+};
+
+module.exports = AuthVerify;
