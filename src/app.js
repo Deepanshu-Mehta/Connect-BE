b4057f2637f3a092fb258ee0e143e32486288851
Deepanshu-Mehta <146458164+Deepanshu-Mehta@users.noreply.github.com>
2025-01-12 00:11:31 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -8,9 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors({
   origin: 'http://localhost:5173/',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  credentials: true
 }));
 
 app.use(express.json());
